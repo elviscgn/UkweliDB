034b53e35b9bcd1e8524051381f71621cbe4299f
elndujhb025 <elndujhb025@student.wethinkcode.co.za>
2026-01-05 13:50:44 +0200

    add error handling module

---
+++ ukweli_db/src/error.rs
@@ -0,0 +1,18 @@
+use core::error;
+
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum LedgerError {
+    #[error("User not registered on Ledger")]
+    UnregistedUser,
+
+    #[error("System error: Could not access previous record")]
+    RecordAccessFailed,
+
+    #[error("Payload cannot be empty")]
+    EmptyPayload,
+
+    #[error("{0}")]
+    ChainValidation(String)
+}
