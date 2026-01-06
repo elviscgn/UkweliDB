aebdf5761eab2e7fd1bc7b2fa790ef6f09c74c0f
elndujhb025 <elndujhb025@student.wethinkcode.co.za>
2026-01-06 17:47:13 +0200

    engine wip

--- ukweli_db/src/workflow/engine.rs
+++ ukweli_db/src/workflow/engine.rs
@@ -1,4 +1,17 @@
+use super::definition::Workflow;
+use super::validators::Validator;
+
+use std::collections::HashMap;
 pub struct Engine {
     pub workflows: Vec<Workflow>,
     pub validators: HashMap<String, Validator>,
+}
+
+impl Engine {
+    pub fn new() -> Self {
+        Self {
+            workflows: Vec::new(),
+            validators: HashMap::new(),
+        }
+    }
 }
