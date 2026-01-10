42ff4fc8ac4493941e872c5bfc5dd4120fd8fa4d
elviscgn <96030189+elviscgn@users.noreply.github.com>
2026-01-10 05:25:01 +0200

    entity state and tracker added

--- ukweli_db/src/entity/state.rs
+++ ukweli_db/src/entity/state.rs
@@ -5,3 +5,14 @@ pub struct EntityState {
     // state_entered_at: String, undecided yet
     pub last_record_index: String,
 }
+
+impl EntityState {
+    pub fn new(id: &str, workflow_id: &str, current_state: &str, last_record_index: usize) -> Self {
+        Self {
+            id: id.to_string(),
+            workflow_id: workflow_id.to_string(),
+            current_state: current_state.to_string(),
+            last_record_index: last_record_index.to_string(),
+        }
+    }
+}
--- ukweli_db/src/entity/tracker.rs
+++ ukweli_db/src/entity/tracker.rs
@@ -3,5 +3,25 @@ use std::collections::HashMap;
 use super::state::EntityState;
 
 pub struct Tracker {
-    entities: HashMap<String, EntityState>,
+    pub entities: HashMap<String, EntityState>,
+}
+
+impl Tracker {
+    pub fn new(
+        entity_id: &str,
+        workflow_id: &str,
+        initial_state: &str,
+        record_index: usize,
+    ) -> Self {
+        let mut entities = HashMap::new();
+        entities.insert(
+            entity_id.to_string(),
+            EntityState::new(entity_id, workflow_id, initial_state, record_index),
+        );
+
+
+        Tracker { entities }
+    }
+
+    
 }
