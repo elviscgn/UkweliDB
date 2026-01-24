7e8e8284cdf730d4b39c5ad45671ca4549f3a5b3
elviscgn <96030189+elviscgn@users.noreply.github.com>
2026-01-24 16:18:34 +0200

    modify user struct so for serialisation prep in the cli

--- ukweli_db/src/core/user.rs
+++ ukweli_db/src/core/user.rs
@@ -39,4 +39,45 @@ impl User {
     pub fn remove_role(&mut self, role: &str) {
         self.roles.remove(role);
     }
+
+
+    // export priv keys as bytes
+    pub fn signing_key_bytes(&self) -> [u8; 32] {
+        self.signing_key.to_bytes()
+    }
+
+    pub fn from_key_bytes(
+        user_id: &str,
+        signing_key_bytes: &[u8; 32],
+        roles: HashSet<String>,
+    ) -> Self {
+        let signing_key = SigningKey::from_bytes(signing_key_bytes);
+        let verifying_key = signing_key.verifying_key();
+
+        User {
+            user_id: user_id.to_owned(),
+            signing_key,
+            verifying_key,
+            roles,
+        }
+    }
+
+    // create a readonly user for verifying only
+    pub fn from_verifying_key(
+        user_id: &str,
+        verifying_key_bytes: &[u8; 32],
+        roles: HashSet<String>,
+    ) -> Result<Self, ed25519_dalek::SignatureError> {
+        let verifying_key = VerifyingKey::from_bytes(verifying_key_bytes)?;
+
+     
+        let signing_key = SigningKey::from_bytes(&[0u8; 32]);
+
+        Ok(User {
+            user_id: user_id.to_owned(),
+            signing_key,
+            verifying_key,
+            roles,
+        })
+    }
 }
