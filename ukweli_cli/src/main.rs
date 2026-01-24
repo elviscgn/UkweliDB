8fa585ccedd66f6ab099a0af9c502577c6a529f0
elviscgn <96030189+elviscgn@users.noreply.github.com>
2026-01-24 08:17:24 +0200

    start work on cli

--- ukweli_cli/src/main.rs
+++ ukweli_cli/src/main.rs
@@ -1,4 +1,14 @@
-// use ukweli_db::add;
+use std::env;
+
+// TODO
+// init – initialise database and genesis record #10
+// record append – append signed records to the ledger #11
+// record verify – verify hash chain and signatures #12
+// workflow load – load JSON/YAML workflow definitions #13
+// workflow list – list available workflows #14
+// record show – inspect records by entity or range #15
+// state current – compute current entity state by replay #16
 fn main() {
-    println!("I got {}", 42);
+    let args = env::args();
+    println!("{:?}", args);
 }
